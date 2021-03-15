e6eccdf10a06d2b2b95ccd613b134859184a8356
sajidmahmud69 <sajidmahmud136@yahoo.com>
2021-03-14 22:36:43 -0400

    final commit

---
+++ tinder-clone/src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../Header.css'
+import PersonIcon from '@material-ui/icons/Person'
+import IconButton from '@material-ui/core/IconButton'
+import ForumIcon from '@material-ui/icons/Forum'
+
+
+function Header() {
+    return (
+        <div className = "header">
+            {/* makes the person icon a ripple affect when you over it */}
+            <IconButton>
+                <PersonIcon fontsize = "large" className = "header__icon"/>
+            </IconButton>
+
+            <img
+                className = "header__logo"
+                src = "https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo.jpg"
+                alt = ""
+            />
+
+            <IconButton>
+                <ForumIcon fontsize = "large" className = "header__icon"/>
+            </IconButton>
+        </div>
+    )
+}
+
+export default Header
